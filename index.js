1f1d8c4a33c0cff9936c43422a0abfb4a6f2068f
shubham kumar <shubhamraj767778@gmail.com>
2025-09-05 00:59:24 +0530

    Project Files

---
+++ index.js
@@ -0,0 +1,71 @@
+let start = false;
+let col = ["red","green","yellow","blue"];
+let GameSeq = [];
+let UserSeq = [];
+
+let level = 0;
+let heading = document.querySelector("h2");
+document.addEventListener("keypress", function () {
+  if (start == false) {
+    console.log("game starded");
+    start = true;
+    levelup();
+  }
+});
+function btnflash(btn) {
+  btn.classList.add("flash");
+  setTimeout(() => {
+    btn.classList.remove("flash");
+  }, 600);
+}
+function Gameflash(btn) {
+  btn.classList.add("gflash");
+  setTimeout(() => {
+    btn.classList.remove("gflash");
+  }, 200);
+}
+///////
+function levelup() {
+    UserSeq=[];
+  level++;
+  heading.innerText = `Level ${level}`;
+  let colindx = Math.floor(Math.random() * 4);
+  let rancol = col[colindx];
+  let randbtn = document.querySelector(`.${rancol}`);
+  GameSeq.push(rancol);
+  
+  console.log(randbtn);
+  btnflash(randbtn);
+}
+function checkAns(idx){
+    if(UserSeq[idx]===GameSeq[idx]){
+        if(UserSeq.length==GameSeq.length){
+            setTimeout(levelup,500);
+        }
+
+    }
+    else{
+        heading.innerHTML=`Game over! Your Score is <b>${level}</b><br> Press any key to restart`;
+        gamerest();
+    }
+}
+
+function btnpress() {
+  let btn = this;
+  Gameflash(btn);
+  let usercol=btn.getAttribute("id"); 
+     console.log(usercol);  
+     UserSeq.push(usercol);
+     checkAns(UserSeq.length-1);
+}
+let btnall = document.querySelectorAll(".btn");
+for (btn of btnall) {
+  btn.addEventListener("click", btnpress);
+}
+function gamerest(){
+    start=false;
+    GameSeq=[];
+    UserSeq=[];
+    level=0;
+
+}
