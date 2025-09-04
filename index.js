c3da9217b3fc18c3890c5c7debf8f73fca46a75d
shubham kumar <shubhamraj767778@gmail.com>
2025-09-05 02:10:41 +0530

    Added start button for mobile support

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 let start = false;
-let col = ["red","green","yellow","blue"];
+let col = ["red", "green", "yellow", "blue"];
 let GameSeq = [];
 let UserSeq = [];
 
 let level = 0;
 let heading = document.querySelector("h2");
-document.addEventListener("keypress", function () {
+let startBtn = document.querySelector("#start-btn"); // Select the new button
+
+// Add a click event listener to the start button
+startBtn.addEventListener("click", function () {
   if (start == false) {
-    console.log("game starded");
+    console.log("game started");
     start = true;
     levelup();
   }
 });
+
+// Remove the old keypress event listener
+// document.addEventListener("keypress", function () {
+//   if (start == false) {
+//     console.log("game starded");
+//     start = true;
+//     levelup();
+//   }
+// });
+
 function btnflash(btn) {
   btn.classList.add("flash");
   setTimeout(() => {
@@ -26,46 +39,43 @@ function Gameflash(btn) {
 }
 ///////
 function levelup() {
-    UserSeq=[];
+  UserSeq = [];
   level++;
   heading.innerText = `Level ${level}`;
   let colindx = Math.floor(Math.random() * 4);
   let rancol = col[colindx];
   let randbtn = document.querySelector(`.${rancol}`);
   GameSeq.push(rancol);
-  
+
   console.log(randbtn);
   btnflash(randbtn);
 }
-function checkAns(idx){
-    if(UserSeq[idx]===GameSeq[idx]){
-        if(UserSeq.length==GameSeq.length){
-            setTimeout(levelup,500);
-        }
-
+function checkAns(idx) {
+  if (UserSeq[idx] === GameSeq[idx]) {
+    if (UserSeq.length == GameSeq.length) {
+      setTimeout(levelup, 500);
     }
-    else{
-        heading.innerHTML=`Game over! Your Score is <b>${level}</b><br> Press any key to restart`;
-        gamerest();
-    }
+  } else {
+    heading.innerHTML = `Game over! Your Score is <b>${level}</b><br> Press any key to restart`;
+    gamerest();
+  }
 }
 
 function btnpress() {
   let btn = this;
   Gameflash(btn);
-  let usercol=btn.getAttribute("id"); 
-     console.log(usercol);  
-     UserSeq.push(usercol);
-     checkAns(UserSeq.length-1);
+  let usercol = btn.getAttribute("id");
+  console.log(usercol);
+  UserSeq.push(usercol);
+  checkAns(UserSeq.length - 1);
 }
 let btnall = document.querySelectorAll(".btn");
 for (btn of btnall) {
   btn.addEventListener("click", btnpress);
 }
-function gamerest(){
-    start=false;
-    GameSeq=[];
-    UserSeq=[];
-    level=0;
-
+function gamerest() {
+  start = false;
+  GameSeq = [];
+  UserSeq = [];
+  level = 0;
 }
